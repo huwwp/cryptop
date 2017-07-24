85a7f32d051605eda50e4fec3f506c77b21a5a7a
= <=>
2017-07-24 17:34:37 +1000

    handled resizing/terminal size errors, implemented file i/o

--- cryptop.py
+++ cryptop.py
@@ -33,47 +33,101 @@ def conf_scr():
 	curses.start_color()
 	curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-	curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_RED)
+	curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_YELLOW)
 
-def write_scr(stdscr, coinl, heldl):
+def write_scr(stdscr, coinl, heldl, dim):
 	'''Write text and formatting to screen'''
-	height, width = stdscr.getmaxyx()
-	stdscr.addstr(0,0,'cryptop v0.1.0', curses.color_pair(0))
-	stdscr.addstr(1,0,'  COIN      PRICE     HELD        VAL     HIGH      LOW  ', curses.color_pair(3))
+	if dim[0] >= 1:
+		stdscr.addnstr(0,0,'cryptop v0.1.0', -1, curses.color_pair(2))
+	if dim[0] >= 2:
+		stdscr.addnstr(1,0,'  COIN      PRICE     HELD        VAL     HIGH      LOW  ', -1, curses.color_pair(3))
+
 	coinvl = getPrice(','.join(coinl))
 	total = 0
-	for coin,val,held in zip(coinl, coinvl, heldl):
-		stdscr.addstr(coinl.index(coin)+2,0,'  {}    {:8.2f} {:8.2f} {:10.2f} {:8.2f} {:8.2f}'.format(coin, val[0], float(held), float(held)*val[0], val[1], val[2]), curses.color_pair(2))
-		total += float(held)*val[0]
-	stdscr.addstr(height-2,0, 'Total Holdings: {:8.2f}'.format(total), curses.color_pair(2))
-	stdscr.addstr(height-1,0, '[A] Add coin [R] Remove coin [0]Exit', curses.color_pair(2))
+
+	if dim[0] > 2:
+		for coin,val,held in zip(coinl, coinvl, heldl):
+			if coinl.index(coin)+2 < dim[0]:
+				stdscr.addnstr(coinl.index(coin)+2,0,'  {}    {:8.2f} {:8.2f} {:10.2f} {:8.2f} {:8.2f}'.format(coin, val[0], float(held), float(held)*val[0], val[1], val[2]), -1, curses.color_pair(2))
+			total += float(held)*val[0]
+	
+	if dim[0] > len(coinl) + 4:
+		stdscr.addnstr(dim[0]-2,0, 'Total Holdings: {:10.2f}    '.format(total), -1, curses.color_pair(3))
+		stdscr.addnstr(dim[0]-1,0, '[A] Add coin [R] Remove coin [0]Exit', -1, curses.color_pair(2))
+	#Comming from below causes issues
 
 def read_file():
 	'''Reads the data file'''
-	with open(datafile) as f:
+	with open(datafile, 'r') as f:
 		data = f.readlines()
 		data = [x.strip() for x in data]
 		coinl, heldl = zip(*(s.split(',') for s in data))
+		coinl = [x for x in coinl]
+		heldl = [x for x in heldl]
 
 	f.close()
 	return coinl, heldl
 
-def write_file():
+def write_file(coinl, heldl):
 	with open(datafile, 'w') as f:
-		print('')
+		for x,y in zip(coinl, heldl):
+			f.write('{},{}\n'.format(x,y))
 
 	f.close()
-	print('')
+
+def get_string(stdscr, prompt):
+	stdscr.nodelay(0)
+	curses.echo()
+	stdscr.clear()
+	stdscr.addnstr(0,0, prompt, -1)
+	curses.curs_set(1)
+	stdscr.refresh()
+	input = stdscr.getstr(1, 0, 20)
+	input = input.decode()
+	curses.noecho()
+	curses.curs_set(1)
+	stdscr.clear()
+	stdscr.nodelay(1)
+	return input
+
+
+def add_coin(input, coinl, heldl):
+	input = input.split(',')
+	coinl.append(input[0])
+	heldl.append(input[1])
+	return coinl, heldl
+
+def rem_coin(input, coinl, heldl):
+	heldl = [x for x in heldl if x.index(x) != coinl.index(input)]
+	coinl = [x for x in coinl if x != input]
+	return coinl, heldl
 
 
 def main(stdscr):
 	inp = 0
 	coinl, heldl = read_file()
+	dim = stdscr.getmaxyx()
 	conf_scr()
 	stdscr.clear()
 	stdscr.nodelay(1)
 	while inp != 48 and inp != 27:
-		write_scr(stdscr, coinl, heldl)
+
+		if dim != stdscr.getmaxyx():
+			stdscr.clear()
+			dim = stdscr.getmaxyx()
+
+		if inp == 97 or inp == 65:
+			data = get_string(stdscr, 'Enter in format Symbol,Ammount e.g. BTC,10')
+			coinl, heldl = add_coin(data, coinl, heldl)
+
+		if inp == 82 or inp == 114:
+			data = get_string(stdscr, 'Enter the symbol of coin to be removed, e.g. BTC')
+			coinl, heldl = rem_coin(data, coinl, heldl)
+
+
+		write_scr(stdscr, coinl, heldl, dim)
 		inp = stdscr.getch()
+
+	write_file(coinl, heldl)
 
 wrapper(main)

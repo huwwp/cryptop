fd1218a0fed8863a9aaabfd469cb8484a03e1717
= <=>
2017-07-25 17:10:55 +1000

    Error handling

--- README.rst
+++ README.rst
@@ -2,3 +2,6 @@ cryptop
 =======
 cryptop is a lightweight command line based cryptocurrency tracker.
 Built on python and ncurses with simplicity in mind.
+
+It currently works in almost all situations but I am ceasing development of this repo port it to C due to
+better curses support.
--- cryptop.py
+++ cryptop.py
@@ -2,8 +2,6 @@ import curses
 from curses import wrapper
 import requests
 import sys
-import time
-
 
 #GLOBALS!
 datafile = '.cryptop'
@@ -38,22 +36,22 @@ def conf_scr():
 def write_scr(stdscr, coinl, heldl, dim):
 	'''Write text and formatting to screen'''
 	if dim[0] >= 1:
-		stdscr.addnstr(0,0,'cryptop v0.1.0', -1, curses.color_pair(2))
+		stdscr.addnstr(0,0,'cryptop v0.1.0', dim[1], curses.color_pair(2))
 	if dim[0] >= 2:
 		stdscr.addnstr(1,0,'  COIN      PRICE     HELD        VAL     HIGH      LOW  ', -1, curses.color_pair(3))
 
 	coinvl = getPrice(','.join(coinl))
 	total = 0
 
-	if dim[0] > 2:
+	if dim[0] > 3:
 		for coin,val,held in zip(coinl, coinvl, heldl):
 			if coinl.index(coin)+2 < dim[0]:
 				stdscr.addnstr(coinl.index(coin)+2,0,'  {}    {:8.2f} {:8.2f} {:10.2f} {:8.2f} {:8.2f}'.format(coin, val[0], float(held), float(held)*val[0], val[1], val[2]), -1, curses.color_pair(2))
 			total += float(held)*val[0]
 	
-	if dim[0] > len(coinl) + 4:
-		stdscr.addnstr(dim[0]-2,0, 'Total Holdings: {:10.2f}    '.format(total), -1, curses.color_pair(3))
-		stdscr.addnstr(dim[0]-1,0, '[A] Add coin [R] Remove coin [0]Exit', -1, curses.color_pair(2))
+	if dim[0] > len(coinl) + 6:
+		stdscr.addnstr(dim[0]-4,0, 'Total Holdings: {:10.2f}    '.format(total), dim[1], curses.color_pair(3))
+		stdscr.addnstr(dim[0]-2,0, '[A] Add coin [R] Remove coin [0]Exit', dim[1], curses.color_pair(2))
 	#Comming from below causes issues
 
 def read_file():
@@ -79,13 +77,13 @@ def get_string(stdscr, prompt):
 	stdscr.nodelay(0)
 	curses.echo()
 	stdscr.clear()
-	stdscr.addnstr(0,0, prompt, -1)
+	stdscr.addnstr(0,0, prompt, -1, curses.color_pair(2))
 	curses.curs_set(1)
 	stdscr.refresh()
 	input = stdscr.getstr(1, 0, 20)
 	input = input.decode()
 	curses.noecho()
-	curses.curs_set(1)
+	curses.curs_set(0)
 	stdscr.clear()
 	stdscr.nodelay(1)
 	return input
@@ -93,8 +91,12 @@ def get_string(stdscr, prompt):
 
 def add_coin(input, coinl, heldl):
 	input = input.split(',')
-	coinl.append(input[0])
-	heldl.append(input[1])
+	if input[0] not in coinl:
+		coinl.append(input[0])
+		heldl.append(input[1])
+	else:
+		heldl[coinl.index(input[0])] = input[1]
+
 	return coinl, heldl
 
 def rem_coin(input, coinl, heldl):
@@ -117,12 +119,14 @@ def main(stdscr):
 			dim = stdscr.getmaxyx()
 
 		if inp == 97 or inp == 65:
-			data = get_string(stdscr, 'Enter in format Symbol,Ammount e.g. BTC,10')
-			coinl, heldl = add_coin(data, coinl, heldl)
+			if dim[0] > 2:
+				data = get_string(stdscr, 'Enter in format Symbol,Ammount e.g. BTC,10')
+				coinl, heldl = add_coin(data, coinl, heldl)
 
 		if inp == 82 or inp == 114:
-			data = get_string(stdscr, 'Enter the symbol of coin to be removed, e.g. BTC')
-			coinl, heldl = rem_coin(data, coinl, heldl)
+			if dim[0] > 2:
+				data = get_string(stdscr, 'Enter the symbol of coin to be removed, e.g. BTC')
+				coinl, heldl = rem_coin(data, coinl, heldl)
 
 
 		write_scr(stdscr, coinl, heldl, dim)
